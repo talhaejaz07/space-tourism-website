640b87f64f6f3cdfb5e73f16b4eb631632879e79
Talha Ejaz <talhaejaz@outlook.com>
2022-11-01 02:06:53 +0500

    font import

--- components/HeadComponent.jsx
+++ components/HeadComponent.jsx
@@ -8,6 +8,12 @@ export const HeadComponent = (props) => {
       <title>{props.title}</title>
       <meta name="description" content={props.description} />
       <link rel="icon" href={Favicon.src} />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Barlow&family=Barlow+Condensed:wght@400;700&family=Bellefair&display=swap"
+        rel="stylesheet"
+      />
     </Head>
   );
 };
